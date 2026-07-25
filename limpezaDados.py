0ffa59b97b7a8dce3323bcaff35360a07c52aab3
bispomariana <msb19112004@gmail.com>
2026-07-24 21:31:18 -0300

    Add data cleaning script for funcionarios.csv

---
+++ limpezaDados.py
@@ -0,0 +1,29 @@
+'''Importando libs e dados'''
+
+import pandas as pd
+
+df_filiais = pd.read_csv("dados/filiais.csv")
+df_departamentos = pd.read_csv("dados/departamentos.csv")
+df_funcionarios = pd.read_csv("dados/funcionarios.csv")
+
+'''Limpando os dados'''
+
+df_funcionarios = df_funcionarios.drop_duplicates()
+
+df_funcionarios['id_reporta_a'] = df_funcionarios['id_reporta_a'].astype('Int64')
+df_funcionarios['data_admissao'] = pd.to_datetime(df_funcionarios['data_admissao'],errors="coerce",format='mixed',dayfirst=True)
+df_funcionarios['data_promocao'] = pd.to_datetime(df_funcionarios['data_promocao'],errors="coerce",format='mixed',dayfirst=True)
+df_funcionarios['nome'] = df_funcionarios['nome'].str.rsplit(' ', n=1).str[0]
+
+df_funcionarios['salario_base'] = df_funcionarios['salario_base'].astype('str')
+
+def padronizar_salarios(valor):
+  valor = valor.replace('R$','')
+  valor = valor.replace(' ','')
+  valor = valor.replace(',','.')
+  return valor
+
+df_funcionarios['salario_base'] = df_funcionarios['salario_base'].apply(padronizar_salarios)
+df_funcionarios['salario_base'] = df_funcionarios['salario_base'].astype('float')
+df_funcionarios['score_satisfacao'] = df_funcionarios.groupby('id_departamento')['score_satisfacao'].transform(lambda x: x.fillna(x.mean())).round(1)
+
